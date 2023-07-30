3495515da3e8565088ab9b8abc343f70bbf528ee
AvazbekDev <126749705+Avazbek-Abduraxmonov@users.noreply.github.com>
2023-07-30 15:13:32 +0500

    function darslari

---
+++ function.py
@@ -0,0 +1,66 @@
+# def function(name,age):
+#     try:
+#         a = age
+#         return a
+#     except ValueError:
+#         print("Dasturda xatolik. Siz son kiritmadingiz")
+# x = function(input("Ismingizni kiriting: "), int(input("Yoshingizni kiriting: ")) - 2023)
+
+# print(x)
+
+# exercise 2
+
+# def function(age):
+#     if age % 2==0:
+#         print(True)
+#     else:
+#         print(False)
+
+# x = function(int(input("Son kiriting: ")))
+
+# print(x)
+
+# exercise 3
+
+# def function(number,numbers):
+#     if number==numbers:
+#         error = "siz kiritgan sonlar teng"
+#         print(error)
+#     else:
+#         error = "Sizning sonlaringiz teng emas"
+#         print(error)
+
+# function(int(input("Sonni kiriting: ")),int(input("Sonni kiriting: ")))
+
+# # print(function)
+
+# print("Dastur tugadi!")
+
+# exercise 4
+
+# def function(x,y):
+#     age = x**y
+
+#     return age
+
+# x=function(int(input("Sonni kiriting: ")),int(input("Sonni kiriting: ")))
+
+# print("Natija: ", x)
+
+# print("dastur tugadi")
+
+# exercise 5
+# -----------
+# oxiriga yetmagan dastur
+# -----------
+
+# def function(number):
+#     if number%2==0 or number%5==0 or number%7==0 or number%10==0:
+#         print("Natija: ", number)
+#     else:
+#         print("Bo'linmaydi: ", number)
+
+
+# x = function(int(input("Sonni kiriting: ")))
+
+# print(x)
